fb1b4dd0eb5c34897a98e6e40e5599d472bb3c6b
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2022-07-23 19:20:03 +0530

    Changed folder structure

---
+++ src/main/java/com/cg/dto/Employee.java
@@ -0,0 +1,27 @@
+package com.cg.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    private String name;
+
+    private String designation;
+
+    private Long salary;
+
+}
--- src/main/java/com/cg/dto/Order.java
+++ src/main/java/com/cg/dto/Order.java
@@ -16,7 +16,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "ordersAA")
+@Table(name = "orders")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
